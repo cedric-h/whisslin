6579f360c5c566de5d8d3fb40fd54326bfb80fb1
RybekU <45723292+RybekU@users.noreply.github.com>
2020-02-27 00:32:19 +0100

    "state" split

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 // #![feature(array_value_iter)]
 
+use crate::gui::InventoryWindow;
 use l8r::L8r;
 use nalgebra as na;
 use ncollide2d::shape::Cuboid;
+use quicksilver::input::Key;
 use quicksilver::{
     geom::Vector,
     graphics::Font,
@@ -111,6 +113,11 @@ impl World {
     }
 }
 
+enum GameState {
+    FARMING,
+    COMBAT,
+}
+
 struct Game {
     last_render: Instant,
     world: World,
@@ -119,34 +126,86 @@ struct Game {
     particle_manager: graphics::particle::Manager,
     gui: gui::GuiState,
     sprite_sheet_animation_failed: bool,
+    state: GameState,
+    entered: bool,
 }
 
 impl State for Game {
     fn new() -> Result<Game> {
         let images = fetch_images();
 
-        let mut world = World::new();
+        Ok(Game {
+            world: World::new(),
+            images,
+            font: Asset::new(Font::load("min.ttf")),
+            particle_manager: Default::default(),
+            gui: gui::GuiState::new(),
+            last_render: Instant::now(),
+            sprite_sheet_animation_failed: false,
+            state: GameState::FARMING,
+            entered: true,
+        })
+    }
+
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        let now = Instant::now();
+        let elapsed = now.duration_since(self.last_render);
+
+        if !self.sprite_sheet_animation_failed {
+            graphics::sprite_sheet::animate(&mut self.world, elapsed).unwrap_or_else(|e| {
+                println!("Disabling sprite sheet animation: {}", e);
+                self.sprite_sheet_animation_failed = true;
+            });
+        }
+        graphics::render(window, &self.world, &mut self.images, &mut self.font)?;
+
+        self.last_render = now;
+        Ok(())
+    }
+
+    fn update(&mut self, window: &mut Window) -> Result<()> {
+        if self.entered {
+            match self.state {
+                GameState::FARMING => self.farming_enter(window),
+                GameState::COMBAT => self.combat_enter(window),
+            }
+            self.entered = false;
+        }
+
+        let transition = match self.state {
+            GameState::FARMING => self.farming_update(window),
+            GameState::COMBAT => self.combat_update(window),
+        };
+        match transition {
+            None => (),
+            Some(GameState::FARMING) => self.farming_exit(window),
+            Some(GameState::COMBAT) => self.combat_exit(window),
+        }
+        match transition {
+            None => (),
+            Some(state) => {
+                self.state = state;
+                self.entered = true;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Game {
+    fn farming_enter(&mut self, _window: &mut Window) {
+        let world = &mut self.world;
         let config = std::rc::Rc::clone(&world.config);
 
-        let player = config.spawn(&mut world);
-        let player_loc = (|| {
-            let PhysHandle(h) = *world.ecs.get::<PhysHandle>(player).ok()?;
-            Some(
-                world
-                    .phys
-                    .collision_object(h)?
-                    .position()
-                    .translation
-                    .vector,
-            )
-        })()
-        .unwrap();
+        let _player = config.spawn(world);
 
         let spear_leaflet = config
             .items
             .get("spear_leaflet")
             .expect("no spear_leaflet in config")
-            .spawn(&mut world);
+            .spawn(world);
+
         world.add_hitbox(
             spear_leaflet,
             Iso2::translation(15.0, 8.5),
@@ -175,81 +234,12 @@ impl State for Game {
             );
         }
 
-        const ENEMY_COUNT: usize = 4;
-        for i in 0..ENEMY_COUNT {
-            let angle = (std::f32::consts::PI * 2.0 / (ENEMY_COUNT as f32)) * (i as f32);
-            let loc = player_loc + na::UnitComplex::from_angle(angle) * Vec2::repeat(5.0);
-            let base_group = CollisionGroups::new().with_membership(&[collide::ENEMY]);
-            let knock_back_not_collide = [collide::ENEMY, collide::PLAYER];
-
-            let bread = world.ecs.spawn((
-                graphics::Appearance {
-                    kind: graphics::AppearanceKind::image("sandwich"),
-                    alignment: graphics::Alignment::Center,
-                    ..Default::default()
-                },
-                combat::health::Health::new(10),
-                combat::DamageReceivedParticleEmitters(vec![config.particles
-                    ["blood_splash"]
-                    .clone()]),
-                DeathParticleEmitters(vec![config.particles["arterial_spray"].clone()]),
-                phys::collision::RigidGroups(base_group.with_blacklist(&knock_back_not_collide)),
-                phys::Charge::new(0.05),
-                phys::LookChase::new(player, 0.025),
-                phys::KnockBack {
-                    groups: base_group.with_whitelist(&knock_back_not_collide),
-                    force_decay: 0.75,
-                    force_magnitude: 0.2,
-                    use_force_direction: false,
-                    minimum_speed: None,
-                },
-            ));
-            world.add_hitbox(
-                bread,
-                Iso2::new(loc, angle),
-                Cuboid::new(Vec2::new(1.0, 1.0) / 2.0),
-                base_group,
-            );
-        }
-
         // Tilemap stuffs
-        tilemap::build_map_entities(&mut world);
-        farm::build_planting_cursor_entity(&mut world);
-
-        Ok(Game {
-            world,
-            images,
-            font: Asset::new(Font::load("min.ttf")),
-            particle_manager: Default::default(),
-            gui: gui::GuiState::new(),
-            last_render: Instant::now(),
-            sprite_sheet_animation_failed: false,
-        })
+        tilemap::build_map_entities(world);
+        farm::build_planting_cursor_entity(world);
     }
-
-    fn draw(&mut self, window: &mut Window) -> Result<()> {
-        let now = Instant::now();
-        let elapsed = now.duration_since(self.last_render);
-
-        if !self.sprite_sheet_animation_failed {
-            graphics::sprite_sheet::animate(&mut self.world, elapsed)
-                .unwrap_or_else(|e| {
-                    println!("Disabling sprite sheet animation: {}", e);
-                    self.sprite_sheet_animation_failed = true;
-                });
-        }
-        graphics::render(
-            window,
-            &self.world,
-            &mut self.images,
-            &mut self.font,
-        )?;
-
-        self.last_render = now;
-        Ok(())
-    }
-
-    fn update(&mut self, window: &mut Window) -> Result<()> {
+    fn farming_exit(&mut self, _window: &mut Window) {}
+    fn farming_update(&mut self, window: &mut Window) -> Option<GameState> {
         #[cfg(feature = "hot-config")]
         self.world.config.reload(&mut self.world);
 
@@ -290,7 +280,114 @@ impl State for Game {
         gui::inventory_events(&mut self.world, &mut self.images);
         items::inventory_inserts(&mut self.world);
 
-        Ok(())
+        if window.keyboard()[Key::N].is_down() {
+            Some(GameState::COMBAT)
+        } else {
+            None
+        }
+    }
+    fn combat_enter(&mut self, _window: &mut Window) {
+        let config = std::rc::Rc::clone(&self.world.config);
+
+        let world = &mut self.world;
+        //
+        let (player, (_player_loc, _)) = world
+            .ecs
+            .query::<(&PhysHandle, &InventoryWindow)>()
+            .iter()
+            .next()
+            .unwrap();
+
+        let player_loc = (|| {
+            let PhysHandle(h) = *world.ecs.get::<PhysHandle>(player).ok()?;
+            Some(
+                world
+                    .phys
+                    .collision_object(h)?
+                    .position()
+                    .translation
+                    .vector,
+            )
+        })()
+        .unwrap();
+
+        const ENEMY_COUNT: usize = 4;
+        for i in 0..ENEMY_COUNT {
+            let angle = (std::f32::consts::PI * 2.0 / (ENEMY_COUNT as f32)) * (i as f32);
+            let loc = player_loc + na::UnitComplex::from_angle(angle) * Vec2::repeat(5.0);
+            let base_group = CollisionGroups::new().with_membership(&[collide::ENEMY]);
+            let knock_back_not_collide = [collide::ENEMY, collide::PLAYER];
+
+            let bread = world.ecs.spawn((
+                graphics::Appearance {
+                    kind: graphics::AppearanceKind::image("sandwich"),
+                    alignment: graphics::Alignment::Center,
+                    ..Default::default()
+                },
+                combat::health::Health::new(10),
+                combat::DamageReceivedParticleEmitters(vec![
+                    config.particles["blood_splash"].clone()
+                ]),
+                DeathParticleEmitters(vec![config.particles["arterial_spray"].clone()]),
+                phys::collision::RigidGroups(base_group.with_blacklist(&knock_back_not_collide)),
+                phys::Charge::new(0.05),
+                phys::LookChase::new(player, 0.025),
+                phys::KnockBack {
+                    groups: base_group.with_whitelist(&knock_back_not_collide),
+                    force_decay: 0.75,
+                    force_magnitude: 0.2,
+                    use_force_direction: false,
+                    minimum_speed: None,
+                },
+            ));
+            world.add_hitbox(
+                bread,
+                Iso2::new(loc, angle),
+                Cuboid::new(Vec2::new(1.0, 1.0) / 2.0),
+                base_group,
+            );
+        }
+    }
+    fn combat_exit(&mut self, _window: &mut Window) {}
+    fn combat_update(&mut self, window: &mut Window) -> Option<GameState> {
+        #[cfg(feature = "hot-config")]
+        self.world.config.reload(&mut self.world);
+
+        graphics::fade::fade(&mut self.world);
+
+        movement::movement(&mut self.world, window);
+        phys::velocity(&mut self.world);
+        phys::chase(&mut self.world);
+        collision::collision(&mut self.world);
+
+        let mouse = window.mouse();
+        let draggable_under_mouse = self.gui.draggable_under(mouse.pos(), &self.world);
+        if draggable_under_mouse.is_some() || self.gui.is_dragging() {
+            self.gui
+                .update_draggable_under_mouse(&mut self.world, draggable_under_mouse, &mouse);
+        } else {
+            aiming::aiming(&mut self.world, window);
+        }
+
+        combat::hurtful_damage(&mut self.world);
+        combat::health::remove_out_of_health(&mut self.world);
+
+        let scheduled_world_edits: Vec<_> = self.world.l8r.drain(..).collect();
+        L8r::now(scheduled_world_edits, &mut self.world);
+
+        self.particle_manager.emit_particles(&mut self.world);
+
+        let scheduled_world_edits: Vec<_> = self.world.l8r.drain(..).collect();
+        L8r::now(scheduled_world_edits, &mut self.world);
+
+        death_particles(&mut self.world);
+        phys::collision::clear_dead_collision_objects(&mut self.world);
+        clear_dead(&mut self.world);
+
+        gui::inventory_events(&mut self.world, &mut self.images);
+        items::inventory_inserts(&mut self.world);
+
+        None
     }
 }
 
